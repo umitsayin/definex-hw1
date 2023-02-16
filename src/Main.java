1668b2b2d55a3789d36f76cda84f06389eb8e56e
Ümit Sayın <umitsayin1661@gmail.com>
2023-02-17 00:07:48 +0300

    end

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,19 @@ import repository.BuildRepository;
 import service.BuildService;
 import service.BuildServiceImpl;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
         final BuildRepository buildRepository = new BuildRepository();
         final List<Build> buildList = buildRepository.getAll();
         final BuildService buildService = new BuildServiceImpl();
+
+        DecimalFormat dfGerman = new DecimalFormat("#,###.##",
+                new DecimalFormatSymbols(Locale.GERMAN));
 
         System.out.println("Total price of houses: " + buildService.getTotalPriceOfHomesInTL(buildList) + CurrencyConstant.TL_CURRENCY);
         System.out.println("Total price of villas: " + buildService.getTotalPriceOfVillasInTL(buildList) + CurrencyConstant.TL_CURRENCY);
@@ -19,10 +25,10 @@ public class Main {
 
         System.out.println("---------------------------------");
 
-        System.out.println("Average square meter of houses: " + buildService.getAverageSquareMeterOfHomes(buildList));
-        System.out.println("Average square meter of villas: " + buildService.getAverageSquareMeterOfVillas(buildList));
-        System.out.println("Average square meter of summeries: " + buildService.getAverageSquareMeterOfSummeries(buildList));
-        System.out.println("Average square meter builds: " + buildService.getAverageSquareMeterOfBuilds(buildList));
+        System.out.printf("Average square meter of houses: %.2f \n", buildService.getAverageSquareMeterOfHomes(buildList));
+        System.out.printf("Average square meter of villas: %.2f \n", buildService.getAverageSquareMeterOfVillas(buildList));
+        System.out.printf("Average square meter of summeries: %.2f \n", buildService.getAverageSquareMeterOfSummeries(buildList));
+        System.out.printf("Average square meter builds: %.2f \n", buildService.getAverageSquareMeterOfBuilds(buildList));
 
         System.out.println("---------------------------------");
 
